39d262fc8545e3e2f8800e6131bfd0be79ded3fe
ryanphadnis <phadrc18@wfu.edu>
2019-09-19 00:04:34 -0400

    merge sort using recursion by Ryan Phadnis

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
+//Author: Ryan Phadnis
+//Course: CSC 112
+//Project 2: Merge Sort
+//9/19/2019
+
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 
 // function declarations
+
+// Divide the function down
 void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right);
+
+//Merge it back up
 void mergeSortedLists(vector<int>& a, vector<int>& tmp, int left, int middle, int right);
 
 int main() {
 
     // get input: first is random seed, second is vector length
     int seed, length;
+    cout << "Type in the seed and length: " << endl;
     cin >> seed >> length;
     srand(seed);
 
     vector<int> v(length);  // vector to be sorted
     vector<int> t(length);  // temporary workspace
     // unit test for merge
-    /* your code here */
+
+    vector<int> test = {47, 23, 71, 6};
+    vector<int> testTwo(4);
+
+    mergeSort(test, testTwo, 0, 3);
+
+    //testing the test array
+    for(int i = 1; i < test.size() - 1; i++) {
+        assert(test.at(i-1) <= test.at(i));
+    }
+
     // initialize and print input
     for(int i = 0; i < v.size(); i++) {
         v.at(i) = rand() % 100;
@@ -26,8 +47,11 @@ int main() {
 
     // sort v
     /* your code here */
+    mergeSort(v, t, 0, v.size() - 1);
 
-    // print output
+    // check output, make sure it's sorted
+
+    // print ending output
     for(int i = 0; i < v.size(); i++) {
         cout << v.at(i) << '\t';
     }
@@ -42,3 +66,63 @@ int main() {
 }
 
 /* your code here */
+void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right)
+{
+    //base case: return if size is 1
+    if(right <= left)
+    {
+        return;
+    }
+    int midpoint = (right + left) / 2;
+    //split into pieces (left)
+    mergeSort(a, tmp, left, midpoint);
+    //split into pieces (right)
+    mergeSort(a, tmp, midpoint + 1, right); //may need a + 1 in here
+    //call the merge function
+    mergeSortedLists(a, tmp, left, midpoint, right);
+}
+
+void mergeSortedLists(vector<int>& a, vector<int>& tmp, int left, int middle, int right)
+{
+    int tleft = left; // left indexes
+    int mid = (middle + 1); // right indexes
+    int tright = left; // temp indexes
+    while (tleft < (middle + 1) && mid < (right + 1))
+    {
+        if (a.at(tleft) < a.at(mid))
+        {
+            tmp.at(tright) = a.at(tleft);
+            tleft++;
+            tright++;
+        }
+        else
+        {
+            tmp.at(tright) = a.at(mid);
+            mid++;
+            tright++;
+        }
+    }
+    if (tleft >= (middle + 1)) //if left list is finished
+        {
+        while (mid <= right)
+        {
+            tmp.at(tright) = a.at(mid);
+            mid++;
+            tright++;
+        }
+    }
+    else if (mid >= right) //if right list is finished
+        {
+        while (tleft < (middle + 1))
+        {
+            tmp.at(tright) = a.at(tleft);
+            tleft++;
+            tright++;
+        }
+    }
+
+    for(int i = left; i <= right; i++)
+    {
+        a.at(i) = tmp.at(i);
+    }
+}
